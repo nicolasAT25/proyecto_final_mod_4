02b0a8847ccad379f8ba7b7066857b7bd832da63
nicolasAT25 <121816436+nicolasAT25@users.noreply.github.com>
2025-09-01 17:32:30 -0500

    Delete Modulo 4 directory

--- Modulo 4/Clase 8/miniProyecto2/controller/inscripcionesController.py
+++
@@ -1,9 +0,0 @@
-from models import inscripcionModel
-from views import inscripcionView
-
-def mostrar_inscripciones():
-    inscripcion = inscripcionModel.consultarInscripciones()
-    if inscripcion:
-        inscripcionView.mostar_inscripciones(inscripcion)
-    else:
-        print("No hay inscripciones registradas.")
